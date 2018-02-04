19d8aa18d19cba37ea46d43993a7c8f9bb830827
Matthew Savoca <msavoca13@gmail.com>
2018-02-04 13:40:24 -0800

    Boxplot and Joy plots working

--- SB_ST_bycatch_sum_viz.R
+++ SB_ST_bycatch_sum_viz.R
@@ -1,25 +1,30 @@
 ################
-#Script for summarizing and visualizing fisheries bycatch data
+#Script for summarizing and visualizing fisheries bycatch data ----
 ################
+
+# Loading packages ----
 library(tidyr)
 library(readxl)
 library(dplyr)
 library(data.table)
-library(ggplot2)
+library(ggridges)
 library(viridis)
 library(tidyverse)
+library(plotly)
 
-#create function for standard error
+
+
+# SE fuction
 SE = function(x){sd(x)/sqrt(sum(!is.na(x)))}
 
-#read in sea turtle and seabird bycatch data
+#read in sea turtle and seabird bycatch data ----
 MMSBST_master10_15 = read.csv("MM_SB_ST_master_data_frame2010_2015.csv")
 
 # makes a column turning bycatch levels into three discrete categories 
 MMSBST_master10_15$Bycatch_level <- ifelse(MMSBST_master10_15$TOTAL.FISHERY.BYCATCH > 1000,"high (>1000)", 
                                            ifelse(MMSBST_master10_15$TOTAL.FISHERY.BYCATCH > 50 & MMSBST_master10_15$TOTAL.FISHERY.BYCATCH < 1000, "moderate (50-1000)", "low (<50)"))
 
-View(MMSBST_master10_15)
+#View(MMSBST_master10_15)
 
 
 # creating a summary table for merging with fish/invert summary table
@@ -28,7 +33,7 @@ d_SBST <- MMSBST_master10_15 %>%
   summarize(Total_Bycatch = mean(TOTAL.FISHERY.BYCATCH)) %>%
   arrange(desc(FISHERY))
 
-View(d_SBST)
+#View(d_SBST)
 
 #Attempt to grab turtle only data
 d_turtles <- d_SBST[d_SBST$GROUP=="sea turtle",] 
@@ -39,7 +44,7 @@ d_turtles <- d_SBST[d_SBST$GROUP=="sea turtle",]
 d_turtles_summary <- aggregate(d_turtles$BYCATCH, by=list(d_turtles$FISHERY, d_turtles$YEAR), FUN=sum)
 colnames(d_turtles_summary) <- c("FISHERY","YEAR","TOTAL_BYCATCH_YEAR_ST")
 d_turtles_2 <- left_join(d_turtles,d_turtles_summary, by = c("FISHERY","YEAR"))
-View(d_turtles_2)
+#View(d_turtles_2)
 
 
 #Now make a seabird dataset
@@ -48,15 +53,15 @@ d_seabird <- d_SBST[d_SBST$GROUP=="seabird",]
 d_seabird_summary <- aggregate(d_seabird$BYCATCH, by=list(d_seabird$FISHERY, d_seabird$YEAR), FUN=sum)
 colnames(d_seabird_summary) <- c("FISHERY","YEAR","TOTAL_BYCATCH_YEAR_SB")
 d_seabird_2 <- left_join(d_seabird,d_seabird_summary, by = c("FISHERY","YEAR"))
-View(d_seabird_2)
+#View(d_seabird_2)
 
 #join sea turtle and seabird bycatch datasets
 d_SBST_2 <- full_join(d_turtles_2, d_seabird_2, by = c("FISHERY","YEAR", "FISHERY.TYPE", "REGION", 
                                                        "GROUP", "BYCATCH", "Bycatch_level", "Total_Bycatch"))
-View(d_SBST_2)
+#View(d_SBST_2)
 
 d_SBST_10_13 <- filter(d_SBST_2, YEAR %in% c("2010", "2011", "2012", "2013") & !FISHERY.TYPE == "")
-View(d_SBST_10_13)
+#View(d_SBST_10_13)
 
 
 
@@ -73,7 +78,7 @@ quantile(poster_master_table$TOTAL_BYCATCH_YEAR_ST, probs = c(0.33, 0.66), na.rm
 quantile(poster_master_table$TOTAL_BYCATCH_YEAR_SB, probs = c(0.33, 0.66), na.rm = TRUE)  # For Seabirds: 99.0 252.8
 quantile(poster_master_table$Bycatch_Ratio, probs = c(0.33, 0.66), na.rm = TRUE)  # For Fish/Inverts: 0.1069364 0.2551906 
 
-#nicer colors
+# nicer color palette ----
 # The palette with grey:
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 # The palette with black:
@@ -108,11 +113,11 @@ SBST <- ggplot(poster_master_table, aes(Bycatch_level)) +
 SBST
 
 
-###############################
+######################################################################
 # Needed to correct some ofthe seabird and sea turtle numbers manually
-###############################
+######################################################################
 
-# New and final plots here:
+# New and final plots here: 
 d_poster_final <- read_csv("poster_master_table_corrected.csv")
 
 
@@ -176,8 +181,9 @@ ST <- ggplot(d_poster_final2, aes(ST_Bycatch_level)) +
         legend.text=element_text(size=10),
         strip.text.x = element_text(size = 12))
 ST
+#ggplotly(ST) # WHOAAA Interactive plot_ly plot
 
-ggsave("Preliminary figures/ST_bycatch_byfishery.pdf", ST)
+#ggsave("Preliminary figures/ST_bycatch_byfishery.pdf", ST)
 
 
 SB <- ggplot(d_poster_final2, aes(SB_Bycatch_level)) +
@@ -196,7 +202,7 @@ SB <- ggplot(d_poster_final2, aes(SB_Bycatch_level)) +
         strip.text.x = element_text(size = 12))
 SB
 
-ggsave("Preliminary figures/SB_bycatch_byfishery.pdf", SB)
+#ggsave("Preliminary figures/SB_bycatch_byfishery.pdf", SB)
 
 d_poster_final3 <- filter(d_poster_final2, !MMPA.Category %in% c(NA, "ND"))
 
@@ -217,7 +223,7 @@ MMPA <- ggplot(d_poster_final3, aes(MMPA.Category)) +
         strip.text.x = element_text(size = 12))
 MMPA
 
-ggsave("Preliminary figures/MMPA_Category_byfishery.pdf", MMPA)
+#ggsave("Preliminary figures/MMPA_Category_byfishery.pdf", MMPA)
 
 
 ## Change label to ordered
@@ -233,7 +239,7 @@ FishInvert <- ggplot(d_poster_final4, aes(BR_level)) +
   guides(fill=guide_legend(title="gear type")) +
   scale_fill_manual(values=cbPalette) +
   #facet_wrap(~YEAR) + 
-  theme_bw()+
+  theme_bw() +
   theme(axis.title.x = element_text(face="bold", size=12),
         axis.text.y  = element_text(size=12),
         axis.text.x = element_text(size=11),
@@ -241,36 +247,80 @@ FishInvert <- ggplot(d_poster_final4, aes(BR_level)) +
         legend.text=element_text(size=10),
         strip.text.x = element_text(size = 12),
         legend.position = "top")
+  
 FishInvert
 
-ggsave("Preliminary figures/FishInvert_bycatch_byfishery.pdf", FishInvert)
+#ggsave("Preliminary figures/FishInvert_bycatch_byfishery.pdf", FishInvert)
 
 
 
 
 
 
-# trying some violin plots of the same data
-FishInvert_V <- ggplot(d_poster_final4, aes(Bycatch_Ratio, FISHERY.TYPE)) +
-  geom_violin(aes(fill = FISHERY.TYPE)) +
-  ylab("Number of fisheries") +
-  xlab("Bycatch ratio of fish and invertebrates") +
-  guides(fill=guide_legend(title="gear type")) +
-  scale_fill_manual(values=cbPalette) +
-  #facet_wrap(~YEAR) + 
-  theme_bw()+
+## trying some box and joy plots of the same data ----
+
+FishInvert_BP = ggplot(d_poster_final4, aes(reorder(FISHERY.TYPE, Bycatch_Ratio, median), Bycatch_Ratio, 
+                       color = FISHERY.TYPE, fill = FISHERY.TYPE)) + 
+  geom_boxplot(alpha = 0.3) +   coord_flip() +
+  geom_jitter(alpha = 0.5, height = 0, width = 0.3)
+
+FishInvert_BP +
+  xlab("Gear Type") +
+  ylab("Bycatch ratio of fish and invertebrates") +
+  theme_bw() +
+  #scale_fill_discrete(values=cbPalette) +
+  ylim(0,1) +
   theme(axis.title.x = element_text(face="bold", size=12),
         axis.text.y  = element_text(size=12),
         axis.text.x = element_text(size=11),
         axis.title.y = element_text(face="bold",size=12),
-        legend.text=element_text(size=10),
-        strip.text.x = element_text(size = 12))
-FishInvert_V
+        legend.position="none")
+
+ggsave("Preliminary figures/FishInvert_boxplot.pdf", FishInvert_BP)
+
+#ggplotly(FishInvert_BP)
+
+# FishInvert_V <- ggplot(d_poster_final4, 
+#                       aes(x=Bycatch_Ratio, y=fct_reorder(FISHERY.TYPE, Bycatch_Ratio), 
+#                           color = FISHERY.TYPE, fill = FISHERY.TYPE)) +
+#   geom_jitter(alpha = 0.5, height = 0.55)
+
+
+# Joy plot, now a density ridge plot ----
+FishInvert_Joy <- ggplot(d_poster_final4, 
+                         aes(x=Bycatch_Ratio, y=reorder(FISHERY.TYPE, Bycatch_Ratio, mean), 
+                             color = FISHERY.TYPE, fill = FISHERY.TYPE)) +
+  geom_point(aes(), alpha = 0.5, position=position_dodge(0.9)) +
+  geom_density_ridges(scale = 1, alpha = 0.5, rel_min_height = 0.01) 
+
+#run separately for whatever dumb reason
+FishInvert_Joy +
+  ylab("Gear Type") +
+  xlab("Bycatch ratio of fish and invertebrates") +
+  theme_bw() +
+  #scale_fill_manual(values=cbPalette) +
+  xlim(0,1) +
+  theme(axis.title.x = element_text(face="bold", size=12),
+        axis.text.y  = element_text(size=12),
+        axis.text.x = element_text(size=11),
+        axis.title.y = element_text(face="bold",size=12),
+        legend.position="none")
+
+ggsave("Preliminary figures/FishInvert_joyplot.pdf", FishInvert_Joy)
+
+# 
+# fct_relevel(FISHERY.TYPE, 
+#             "jig", "seine", "line", "pot", "dredge", 
+#             "gillnet", "longline", "trawl")
+
+ggplotly(FishInvert_Joy)
+
+
 
 
 
 ############
-# Junk code
+# Junk code ----
 ############
 
 # THIS IS PRODUCING SOME REAL SCREWY RESULTS RN
@@ -284,3 +334,35 @@ View(master_table)
 
 poster_master_table$Total_Bycatch.y[is.na(poster_master_table$Total_Bycatch.y)] <- 0
 View(poster_master_table)
+
+# Doesn't work because - Warning message:position_dodge requires non-overlapping x intervals
+# FishInvert_V +
+#   geom_boxplot(aes(d_poster_final4$Bycatch_Ratio), alpha = 0.5)
+# 
+# FishInvert_V
+# 
+# d_poster_final4$FISHERY.TYPE <- as.factor(d_poster_final4$FISHERY.TYPE)
+# # does any violin plot work?
+# V_try <-  ggplot(aes(x=FISHERY.TYPE, y=Bycatch_Ratio), data = d_poster_final4) +
+#                   geom_violin()+
+#                   coord_flip()
+# 
+# ML_V = ggplot(Mass_loss, aes(x = Stage, y = log(Mass.loss..g.), fill = Stage, colour = Stage)) +
+#   geom_violin(alpha = 0.3)
+
+
+
+geom_histogram(alpha = 0.5) +
+  ylab("Gear Type") +
+  xlab("Bycatch ratio of fish and invertebrates") +
+  guides(fill=guide_legend(title="gear type")) +
+  scale_fill_manual(values=cbPalette) +
+  #facet_wrap(~YEAR) + 
+  theme_bw()+
+  theme(axis.title.x = element_text(face="bold", size=12),
+        axis.text.y  = element_text(size=12),
+        axis.text.x = element_text(size=11),
+        axis.title.y = element_text(face="bold",size=12),
+        legend.text=element_text(size=10),
+        strip.text.x = element_text(size = 12))
+FishInvert_Joy
